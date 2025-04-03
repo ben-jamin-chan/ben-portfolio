d2313d63f0a0d1db86bcc9217f2d52153e26a563
ben-jamin-chan <chanbenjamin.tl@gmail.com>
2025-04-04 00:17:10 +0800

    Configured for GitHub Pages #2

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,6 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  // base: mode === "production" ? "/ben-portfolio" : "/",
   base: "/ben-portfolio/",
   server: {
     host: "::",
