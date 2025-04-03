d168027b38844cb520c8b7964be92cb04483882c
ben-jamin-chan <chanbenjamin.tl@gmail.com>
2025-04-03 23:52:34 +0800

    Configured for GH Pages

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,8 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: mode === "production" ? "/ben-portfolio" : "/",
+  // base: mode === "production" ? "/ben-portfolio" : "/",
+  base: "/ben-portfolio/",
   server: {
     host: "::",
     port: 8080,
