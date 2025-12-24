40c18b130933ae7046d0b5720e2d2daa12e9ba1c
ben-jamin-chan <chanbenjamin.tl@gmail.com>
2025-12-25 00:54:04 +0800

    Updated tailwind.config.ts

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -4,11 +4,11 @@ import type { Config } from "tailwindcss";
 export default {
 	darkMode: ["class"],
 	content: [
-		"./pages/**/*.{js,jsx}",
-		"./components/**/*.{js,jsx}",
-		"./app/**/*.{js,jsx}",
-		"./src/**/*.{js,jsx}",
-	],
+		"./pages/**/*.{js,jsx,ts,tsx}",
+		"./components/**/*.{js,jsx,ts,tsx}",
+		"./app/**/*.{js,jsx,ts,tsx}",
+		"./src/**/*.{js,jsx,ts,tsx}",
+	  ],
 	prefix: "",
 	theme: {
 		container: {
